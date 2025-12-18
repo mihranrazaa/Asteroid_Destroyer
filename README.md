847f3e2e2d27e97c289ef278c9bab691d43daa07
mihranrazaa <mihranrazaa@protonmail.com>
2025-12-19 05:03:30 +0530

    Initial update Asteroid_Destroyer_V1

---
+++ README.md
@@ -0,0 +1,37 @@
+# Asteroid Destroyer?
+
+---
+
+## Introduction
+
+A game where you control a spaceship and destroy asteroids. This is my first ever game. It is using pygame and pygame_GUI, the game gets over when you collide with an asteroid. 
+
+## Controls 
+
+W -- Move forward
+
+A -- Rotate left
+
+D -- Rotate right
+
+S -- Move backward
+
+Space -- Shoot
+
+## How to Play
+
+Just download the Compiled version of the game from the releases page or download the source code from Github.
+
+## BTS
+
+- The triangle is also a circle in the game logic, so it can be easier for me to implement collision detection as a newbie.
+- I dunno what to write here..
+- I can't decide what name i should give to the game, so i gave the BEST GENERIC NAME ::::: ASTEROID DESTROYER!!!
+
+## Credits 
+
+- Creator of pygame: https://www.pygame.org/
+
+## License
+
+- MIT License: https://opensource.org/licenses/MIT
---
+++ main.py
@@ -0,0 +1,66 @@
+"""
+Project By - mihranrazaa
+Date - 18-12-2025
+License - None
+Name - Not yet Decided :?
+"""
+
+import sys
+
+import pygame
+
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_event, log_state
+from player import Player
+from shot import Shot
+
+#
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    pygame.init()
+    refresh = pygame.time.Clock()
+    dt = 0
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    AsteroidField()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        pygame.Surface.fill(screen, "black")
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        for asteroid in list(asteroids):
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in list(shots):
+                if shot.collides_with(asteroid):
+                    if asteroid.alive() and shot.alive():
+                        log_event("asteroid_shot")
+                        asteroid.split()
+                        shot.kill()
+
+        pygame.display.flip()
+        dt = refresh.tick(60) / 1000.0
+
+
+main()
